c1e8fb9a47495ac24d526ce0dc26c446cdecdd07
risav pyakurel <pyakurel.risav@gmail.com>
2024-03-31 00:05:00 +0545

    nothing

---
+++ hamrocart/item/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Category,Item
+
+# Register your models here.
+
+from.models import Category
+
+admin.site.register(Category)
+admin.site.register(Item)
